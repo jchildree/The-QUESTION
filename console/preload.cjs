e70c7efaabe55c7e95f3613488f25425c05036e9
Joseph Childree <jchildree87@gmail.com>
2026-07-15 19:57:12 -0600

    feat: implement preload.cjs contextBridge with vault and claude IPC

--- console/preload.cjs
+++ console/preload.cjs
@@ -1,6 +1,31 @@
 "use strict";
 
-const { contextBridge } = require("electron");
+const { contextBridge, ipcRenderer } = require("electron");
 
-// Stub - will be populated in a later task
-contextBridge.exposeInMainWorld("electronAPI", {});
+contextBridge.exposeInMainWorld("electronAPI", {
+  // Vault operations
+  openVault: () => ipcRenderer.invoke("vault:open"),
+  readVault: () => ipcRenderer.invoke("vault:read"),
+  writeFile: (fileName, content) =>
+    ipcRenderer.invoke("vault:write", { fileName, content }),
+  watchVault: () => ipcRenderer.invoke("vault:watch"),
+  onVaultChanged: (callback) => {
+    ipcRenderer.on("vault:changed", (_event, data) => callback(data));
+  },
+
+  // Claude operations
+  queryClaude: (messages, boardContext, apiKey) =>
+    ipcRenderer.invoke("claude:query", { messages, boardContext, apiKey }),
+  onClaudeChunk: (callback) => {
+    ipcRenderer.on("claude:chunk", (_event, data) => callback(data));
+  },
+  onClaudeDone: (callback) => {
+    ipcRenderer.on("claude:done", (_event, data) => callback(data));
+  },
+  onClaudeError: (callback) => {
+    ipcRenderer.on("claude:error", (_event, data) => callback(data));
+  },
+
+  // Cleanup - remove all listeners for a channel
+  removeListeners: (channel) => ipcRenderer.removeAllListeners(channel),
+});
